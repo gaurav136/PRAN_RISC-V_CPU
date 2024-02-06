94ce1773cfcc1e8fbcf4d5c16c4bf2fa6f56b325
gaurav136 <gauravtale136@gmail.com>
2024-02-06 16:58:17 +0530

    aluslt, sltu modified

--- PRAN/components/alu.v
+++ PRAN/components/alu.v
@@ -10,19 +10,19 @@ module alu #(parameter WIDTH = 32)
     
     always @(a, b, alu_ctrl) begin
         case (alu_ctrl)
-            4'b0000:  alu_out <= a + b;       // ADD
-            4'b1000:  alu_out <= a + ~b + 1;  // SUB
-            4'b0001:  alu_out <= a << {27'b0,b[4:0]}; //SLL
-            4'b0010:  begin                   // SLT
-                if (a[31] ! = b[31]) alu_out < = a[31] ? 0 : 1;
+            4'b0000:  alu_out <= a + b;       						// ADD
+            4'b1000:  alu_out <= a + ~b + 1;  						// SUB
+            4'b0001:  alu_out <= a << {27'b0,b[4:0]}; 			//SLL
+            4'b0010:  begin                   						// SLT
+                if (a[31] != b[31]) alu_out <= a[31] ? 1 : 0;
                 else alu_out <= a < b ? 1 : 0;
             end
-            //4'b0011:
-            4'b0100: alu_out <= a ^ b; //XOR
-            4'b0101: alu_out <= a >> {27'b0,b[4:0]}; //SRL
-            4'b1101: alu_out <= a >>> {27'b0,b[4:0]}; //SRA
-            4'b0110: alu_out <= a | b; //OR
-            4'b0111: alu_out <= a & b; //AND
+            4'b0011:	alu_out <= a < b ? 1 : 0; 						//SLTU
+            4'b0100: alu_out <= a ^ b; 								//XOR
+            4'b0101: alu_out <= a >> {27'b0,b[4:0]};				//SRL
+            4'b1101: alu_out <= a >>> {27'b0,b[4:0]}; 			//SRA
+            4'b0110: alu_out <= a | b; 								//OR
+            4'b0111: alu_out <= a & b; 								//AND
             
             default: alu_out = 0;
         endcase
